0304574fb157e116959ffad5c1deaced196a80f2
SanderV1992 <sander.vergeles@gmail.com>
2018-09-13 08:47:52 +0300

    first commit

---
+++ src/main/java/demo/springapplication/demoSpringapplication/DemoSpringapplicationApplication.java
@@ -0,0 +1,12 @@
+package demo.springapplication.demoSpringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringapplicationApplication.class, args);
+	}
+}
